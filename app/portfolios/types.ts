0fe793024bd15e947aa693fb5bb35762f76ba0dd
chiragxy <133590226+chiragxy@users.noreply.github.com>
2025-04-17 22:14:13 +0530

    Create types.ts

---
+++ app/portfolios/types.ts
@@ -0,0 +1,7 @@
+export interface Portfolio {
+    id: string; // Unique identifier
+    name: string;
+    title: string;
+    description: string;
+    imageUrl?: string; // Optional image URL
+  }
